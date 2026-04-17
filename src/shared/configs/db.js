f4bc144df0b4e5d0264f68e220acb21e5dc34e84
ShazaibSyed435 <syedshazaibzaheer@example.com>
2026-04-17 21:34:24 +0500

    i

--- src/shared/configs/db.js
+++ src/shared/configs/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
       throw new Error("MONGODB_URI is not defined in .env");
     }
 
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await    mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
